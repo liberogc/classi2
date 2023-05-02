89145401f41d4ed0ad0d132598e525c2d86b99f6
Giovanni <53482321+liberogc@users.noreply.github.com>
2023-05-02 19:30:09 +0200

    commit_-2

--- Classi2/src/principale/Test.java
+++ Classi2/src/principale/Test.java
@@ -3,7 +3,7 @@ package principale;
 import classi.Generica;
 
 public class Test {
-
+	// progetto classi
 	public static void main(String[] args) {
 		Generica gen = new Generica();
 		gen.somma();
